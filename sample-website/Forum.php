fdc586ab8e82d5084df4195f435a066c345035e6
dkaitou <dkaitou@hotmail.com>
2018-10-23 08:43:15 +1000

    Update forum.php
    Removed the redundant spaces

--- sample-website/Forum.php
+++ sample-website/Forum.php
@@ -1,12 +1,16 @@
 <?php
+
 class Forum
 {
     const REMEMBER_ME_KEY = 'flarum_remember';
+
     private $config;
+
     public function __construct()
     {
         $this->config = require __DIR__ . '/config.php';
     }
+
     /**
      * Call this method after your user is successfully authenticated.
      *
@@ -18,28 +22,17 @@ class Forum
         if($password == ''){
 			$password = $this->createPassword($username);
 		}
+		
         $token = $this->getToken($username, $password);
+
         if (empty($token)) {
             $this->signup($username, $password, $email);
             $token = $this->getToken($username, $password);
         }
+
         $this->setRememberMeCookie($token);
     }
-    /**
-     * Call this method after you logged out your user.
-     */
-    public function logout()
-    {
-        $this->removeRememberMeCookie();
-    }
-    /**
-     * Redirects a user back to the forum.
-     */
-    public function redirectToForum()
-    {
-        header('Location: ' . $this->config['flarum_url']);
-        die();
-    }
+	
 	/**
      * Reset the password for forum user
      */
@@ -56,10 +49,29 @@ class Forum
         ];
         $response = $this->sendPatchRequest('/api/users/'. $userObj['data']['id'], $data);
     }
+
+	/**
+     * Call this method after you logged out your user.
+     */
+    public function logout()
+    {
+        $this->removeRememberMeCookie();
+    }
+
+    /**
+     * Redirects a user back to the forum.
+     */
+    public function redirectToForum()
+    {
+        header('Location: ' . $this->config['flarum_url']);
+        die();
+    }
+
     private function createPassword($username)
     {
         return hash('sha256', $username . $this->config['password_token']);
     }
+
     private function getToken($username, $password)
     {
         $data = [
@@ -67,9 +79,12 @@ class Forum
             'password' => $password,
             'lifetime' => $this->getLifetimeInSeconds(),
         ];
+
         $response = $this->sendPostRequest('/api/token', $data);
+
         return isset($response['token']) ? $response['token'] : '';
     }
+
     private function signup($username, $password, $email)
     {
         $data = [
@@ -82,12 +97,16 @@ class Forum
                 ]
             ]
         ];
+
         $response = $this->sendPostRequest('/api/users', $data);
+
         return isset($response['data']['id']);
     }
+
     private function sendPostRequest($path, $data)
     {
         $data_string = json_encode($data);
+
         $ch = curl_init($this->config['flarum_url'] . $path);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
@@ -99,8 +118,10 @@ class Forum
             ]
         );
         $result = curl_exec($ch);
+
         return json_decode($result, true);
     }
+	
 	private function sendGetRequest($path)
     {
 		echo $this->config['flarum_url'] . $path;
@@ -117,6 +138,7 @@ class Forum
         $result = curl_exec($ch);
         return json_decode($result, true);
     }
+	
 	private function sendPatchRequest($path, $data)
     {
 		echo $this->config['flarum_url'] . $path;
@@ -135,23 +157,28 @@ class Forum
         $result = curl_exec($ch);
         return json_decode($result, true);
     }
+
     private function setRememberMeCookie($token)
     {
         $this->setCookie(self::REMEMBER_ME_KEY, $token, time() + $this->getLifetimeInSeconds());
     }
+
     private function removeRememberMeCookie()
     {
         unset($_COOKIE[self::REMEMBER_ME_KEY]);
         $this->setCookie(self::REMEMBER_ME_KEY, '', time() - 10);
     }
+
     private function setCookie($key, $token, $time)
     {
         setcookie($key, $token, $time, '/', $this->config['root_domain']);
     }
+
     private function getLifetimeInSeconds()
     {
         return $this->config['lifetime_in_days'] * 60 * 60 * 24;
     }
+	
 	private function getUserDetails($username){
     	$response = $this->sendGetRequest('/api/users/'.$username);
     	return $response;
